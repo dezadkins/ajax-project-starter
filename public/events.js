c73f433fd8be0c22f558f1647ec217f21fb547e6
dezadkins <desmondadkins@gmail.com>
2020-10-06 10:54:43 -0700

    phase 2 done

--- public/events.js
+++ public/events.js
@@ -1,6 +1,14 @@
 const handleClick = () => {
     fetch ('/kitten/image')
     .then(res => {
+        console.log("this is the response",res)
         return res.json();
+
+    }).then (json => {
+        console.log("this is the json obj",json);
+        document.querySelector(".cat-pic").src = json.src;
+
     })
 }
+
+document.getElementById("new-pic").addEventListener('click', handleClick);
