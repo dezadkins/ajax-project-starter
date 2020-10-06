2a41d70c4a507b4f98e19b309524d51edffb622c
Adam <warwickjunglex@gmail.com>
2020-10-06 16:00:55 -0400

    Implemented up/downvote

--- public/events.js
+++ public/events.js
@@ -1,14 +1,60 @@
 const handleClick = () => {
+    document.querySelector(".loader").innerHTML = "Loading...";
     fetch ('/kitten/image')
     .then(res => {
-        console.log("this is the response",res)
+        if(!res.ok) {
+            throw res;
+        }
+        // console.log("this is the response",res)
         return res.json();
-
     }).then (json => {
-        console.log("this is the json obj",json);
+        // console.log("this is the json obj",json);
         document.querySelector(".cat-pic").src = json.src;
-
+        document.querySelector(".loader").innerHTML = "";
+    })
+    .catch(err => {
+        err.json().then(errorJSON => {
+            document.querySelector(".error").innerHTML = errorJSON.message;
+        })
     })
 }
 
 document.getElementById("new-pic").addEventListener('click', handleClick);
+
+const upvote = () => {
+    fetch('/kitten/upvote', { method: 'PATCH' })
+        .then(res => {
+            if(!res.ok) {
+                throw res;
+            }
+            return res.json();
+        })
+        .then(data => {
+            document.querySelector(".score").innerHTML = data.score;
+        })
+        .catch(err => {
+            err.json().then(errJSON => {
+                console.log(errJSON);
+            })
+        })
+}
+const downvote = () => {
+    fetch('/kitten/downvote', { method: 'PATCH' })
+        .then(res => {
+            if(!res.ok) {
+                throw res;
+            }
+            return res.json();
+        })
+        .then(data => {
+            document.querySelector(".score").innerHTML = data.score;
+        })
+        .catch(err => {
+            err.json().then(errJSON => {
+                console.log(errJSON);
+            })
+        })
+}
+
+document.getElementById("upvote").addEventListener("click", upvote);
+document.getElementById("downvote").addEventListener("click", downvote);
